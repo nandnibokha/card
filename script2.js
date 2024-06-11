2c8a26b8690d3b4ce160e230b12afbee820d1dee
nandnibokha <159996046+nandnibokha@users.noreply.github.com>
2024-06-11 11:38:12 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,161 @@
+let student1 = {
+    sr : 1,
+    name : 'Nandni',
+    age : 19,
+    hobbies : ['Dancing, Travelling'],
+    contact : 9327194199,
+}
+let student2 = {
+    sr : 2,
+    name : 'Radhi',
+    age : 18,
+    hobbies : ['Travelling, Singing'],
+    contact : 7362839482,
+}
+let student3 = {
+    sr : 3,
+    name : 'Astha',
+    age : 18,
+    hobbies : ['Travelling'],
+    contact : 7452548562,
+}
+let student4 = {
+    sr : 4,
+    name : 'Dhara',
+    age : 18,
+    hobbies : ['Dancing, Travelling'],
+    contact : 6583745628,
+}
+let student5 = {
+    sr : 5,
+    name : 'Dipali',
+    age : 19,
+    hobbies : ['Walking, Playing'],
+    contact : 8462585835,
+}
+let student6 = {
+    sr : 6,
+    name : 'Urvisha',
+    age : 19,
+    hobbies : ['Playing'],
+    contact : 8573639573,
+}
+let student7 = {
+    sr : 7,
+    name : 'Priya',
+    age : 17,
+    hobbies : ['Travelling, Eating'],
+    contact : 9573637583,
+}
+let student8 = {
+    sr : 8,
+    name : 'Ragini',    
+    age : 17,
+    hobbies : ['Sleeping, Eating'],
+    contact : 6573827465,
+}
+let student9 = {
+    sr : 9,
+    name : 'Denisha',
+    age : 19,
+    hobbies : ['Eating, Fighting'],
+    contact : 9577635264,
+}
+let student10 = {
+    sr : 10,
+    name : 'Heny',
+    age : 19,
+    hobbies : ['Singing'],
+    contact : 9472527885,
+}
+let student11 = {
+    sr : 11, 
+    name : 'Akshali',
+    age : 16,
+    hobbies : ['Dancing'],
+    contact : 8673648673,
+}
+let student12 = {
+    sr : 12,
+    name : 'Aarya',
+    age : 17,
+    hobbies : ['Travelling, Singing'],
+    contact : 7562859276,
+}
+let student13 = {
+    sr : 13,
+    name : 'Purvi',
+    age : 21,
+    hobbies : ['Travelling, Playing'],
+    contact : 9672759372,
+}
+let student14 = {
+    sr : 14,
+    name : 'Priyanshi',
+    age : 20,
+    hobbies : ['Dancing, Travelling'],
+    contact : 9673749697,
+}
+let student15 = {
+    sr : 15,
+    name : 'archi',
+    age : 22,
+    hobbies : ['Dancing, Playing'],
+    contact : 7573399375,
+}
+let student16 = {
+    sr : 16,
+    name : 'Archi',
+    age : 18,
+    hobbies : ['Dancing, Travelling'],
+    contact : 7563758365,
+}
+let student17 = {
+    sr : 17,
+    name : 'Bhumika',
+    age : 23,
+    hobbies : ['Travelling, Working'],
+    contact : 7582947659,
+}
+let student18 = {
+    sr : 18,
+    name : 'Aesha',    
+    age : 16,
+    hobbies : ['Eating'],
+    contact : 7682947672,
+}
+let student19 = {
+    sr : 19,
+    name : 'Krisha',
+    age : 21,
+    hobbies : ['Playing'],
+    contact : 6582957599,
+}
+let student20 = {
+    sr : 20,
+    name : 'Bhumi',
+    age : 24,
+    hobbies : ['Singing, Walking'],
+    contact : 8461492543,
+}
+
+document.querySelector('.card-text1').innerHTML = "Sr.no : " + student1.sr + "<br>" + "Name : " + student1.name + "<br>" + "Age : " + student1.age + "<br>" + "Hobbies : " + student1.hobbies + "<br>" + "Contact : " + student1.contact;
+document.querySelector('.card-text2').innerHTML = "Sr.no : " + student2.sr + "<br>" + "Name : " + student2.name + "<br>" + "Age : " + student2.age + "<br>" + "Hobbies : " + student2.hobbies + "<br>" + "Contact : " + student2.contact;
+document.querySelector('.card-text3').innerHTML = "Sr.no : " + student3.sr + "<br>" + "Name : " + student3.name + "<br>" + "Age : " + student3.age + "<br>" + "Hobbies : " + student3.hobbies + "<br>" + "Contact : " + student3.contact;
+document.querySelector('.card-text4').innerHTML = "Sr.no : " + student4.sr + "<br>" + "Name : " + student4.name + "<br>" + "Age : " + student4.age + "<br>" + "Hobbies : " + student4.hobbies + "<br>" + "Contact : " + student4.contact;
+document.querySelector('.card-text5').innerHTML = "Sr.no : " + student5.sr + "<br>" + "Name : " + student5.name + "<br>" + "Age : " + student5.age + "<br>" + "Hobbies : " + student5.hobbies + "<br>" + "Contact : " + student5.contact;
+document.querySelector('.card-text6').innerHTML = "Sr.no : " + student6.sr + "<br>" + "Name : " + student6.name + "<br>" + "Age : " + student6.age + "<br>" + "Hobbies : " + student6.hobbies + "<br>" + "Contact : " + student6.contact;
+document.querySelector('.card-text7').innerHTML = "Sr.no : " + student7.sr + "<br>" + "Name : " + student7.name + "<br>" + "Age : " + student7.age + "<br>" + "Hobbies : " + student7.hobbies + "<br>" + "Contact : " + student7.contact;
+document.querySelector('.card-text8').innerHTML = "Sr.no : " + student8.sr + "<br>" + "Name : " + student8.name + "<br>" + "Age : " + student8.age + "<br>" + "Hobbies : " + student8.hobbies + "<br>" + "Contact : " + student8.contact;
+document.querySelector('.card-text9').innerHTML = "Sr.no : " + student9.sr + "<br>" + "Name : " + student9.name + "<br>" + "Age : " + student9.age + "<br>" + "Hobbies : " + student9.hobbies + "<br>" + "Contact : " + student9.contact;
+document.querySelector('.card-text10').innerHTML = "Sr.no : " + student10.sr + "<br>" + "Name : " + student10.name + "<br>" + "Age : " + student10.age + "<br>" + "Hobbies : " + student10.hobbies + "<br>" + "Contact : " + student10.contact;
+document.querySelector('.card-text11').innerHTML = "Sr.no : " + student11.sr + "<br>" + "Name : " + student11.name + "<br>" + "Age : " + student11.age + "<br>" + "Hobbies : " + student11.hobbies + "<br>" + "Contact : " + student11.contact;
+document.querySelector('.card-text12').innerHTML = "Sr.no : " + student12.sr + "<br>" + "Name : " + student12.name + "<br>" + "Age : " + student12.age + "<br>" + "Hobbies : " + student12.hobbies + "<br>" + "Contact : " + student12.contact;
+document.querySelector('.card-text13').innerHTML = "Sr.no : " + student13.sr + "<br>" + "Name : " + student13.name + "<br>" + "Age : " + student13.age + "<br>" + "Hobbies : " + student13.hobbies + "<br>" + "Contact : " + student13.contact;
+document.querySelector('.card-text14').innerHTML = "Sr.no : " + student14.sr + "<br>" + "Name : " + student14.name + "<br>" + "Age : " + student14.age + "<br>" + "Hobbies : " + student14.hobbies + "<br>" + "Contact : " + student14.contact;
+document.querySelector('.card-text15').innerHTML = "Sr.no : " + student15.sr + "<br>" + "Name : " + student15.name + "<br>" + "Age : " + student15.age + "<br>" + "Hobbies : " + student15.hobbies + "<br>" + "Contact : " + student15.contact;
+document.querySelector('.card-text16').innerHTML = "Sr.no : " + student16.sr + "<br>" + "Name : " + student16.name + "<br>" + "Age : " + student16.age + "<br>" + "Hobbies : " + student16.hobbies + "<br>" + "Contact : " + student16.contact;
+document.querySelector('.card-text17').innerHTML = "Sr.no : " + student17.sr + "<br>" + "Name : " + student17.name + "<br>" + "Age : " + student17.age + "<br>" + "Hobbies : " + student17.hobbies + "<br>" + "Contact : " + student17.contact;
+document.querySelector('.card-text18').innerHTML = "Sr.no : " + student18.sr + "<br>" + "Name : " + student18.name + "<br>" + "Age : " + student18.age + "<br>" + "Hobbies : " + student18.hobbies + "<br>" + "Contact : " + student18.contact;
+document.querySelector('.card-text19').innerHTML = "Sr.no : " + student19.sr + "<br>" + "Name : " + student19.name + "<br>" + "Age : " + student19.age + "<br>" + "Hobbies : " + student19.hobbies + "<br>" + "Contact : " + student19.contact;
+document.querySelector('.card-text20').innerHTML = "Sr.no : " + student20.sr + "<br>" + "Name : " + student20.name + "<br>" + "Age : " + student20.age + "<br>" + "Hobbies : " + student20.hobbies + "<br>" + "Contact : " + student20.contact;
